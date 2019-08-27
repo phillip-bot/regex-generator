f9baf69be8f5526e991825a143e9a1e443803484
Greg Chan <gregorywarrenchan@gmail.com>
2019-08-26 17:16:48 -0700

    fix(index): add jsdoc to index

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 
 const gp = require('./gp');
 
+/**
+ * Generates a regex from a map of examples
+ * @param {Map<String, String>} examples - a map of examples, where the key is the string and the value is the substring to match
+ * @param {Object} options - configuration options
+ * @param {Boolean} options.weightedSize - a flag to uses weighted sizes in genetic programming
+ * @param {Boolean} options.guessRegex - guess the regex using the examples and use this as seed data for the genetic program
+ * @param {Array<Node>} options.population - a seed population
+ * @param {Number} options.populationSize - the size of a seed population
+ * @param {Number} options.mutationWeight - percentage as a decimal of the size of the population to mutate
+ * @param {Number} options.crossoverWeight - percentage as a decimal of the size of the population to crossover
+ * @param {Number} options.replicationWeight - percentage as a decimal of the size of the population to replicate to the next
+ * @param {Number} options.iterationsThreshold - number of iterations to determine convergence
+ * @param {Number} options.deltaThreshold - the score difference between best canidates that will result in termination
+ *
+ * @return {Object} with seed and regex
+ */
 const regexFromExamples = function (examples, options) {
   return gp.run(examples, options);
 };
